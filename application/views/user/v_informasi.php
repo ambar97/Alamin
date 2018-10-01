056f0babe0cab23cf725ca93bf12be377a775dbb
Christopher Nanda Jonathan <cnjonathan.cnj@gmail.com>
2018-10-01 22:17:21 +0700

    DATABASE disamakan semua "sdaba02jember"

--- application/views/user/v_informasi.php
+++ application/views/user/v_informasi.php
@@ -43,73 +43,23 @@
 								</div>
 							</div>
 							<div class="col-lg-7">
-
 								<div class="post-content">
-
 									<h2><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
 									<p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
-
 								</div>
 							</div>
-
 						</div>
 						<div class="row">
 							<div class="col">
 								<div class="post-meta">
 									<span><i class="fas fa-calendar-alt"></i> January 10, 2017 </span>
-									<span><i class="fas fa-user"></i> By <a href="#">John Doe</a> </span>
-									<span><i class="fas fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
-									<span><i class="fas fa-comments"></i> <a href="#">12 Comments</a></span>
-									<span class="d-block d-md-inline-block float-md-right mt-3 mt-md-0"><a href="blog-post.html" class="btn btn-xs btn-primary">Read more...</a></span>
+									<span><i class="fas fa-comments"></i> <a href="#">12 viewers</a></span>
+									<span class="d-block d-md-inline-block float-md-right mt-3 mt-md-0"><a href="<?php echo base_url('Informasi/Berita_Detail'); ?>" class="btn btn-xs btn-primary">Read more...</a></span>
 								</div>
 							</div>
 						</div>
-
 					</article>
 					<hr>
-					<article class="post post-medium">
-						<div class="row">
-
-							<div class="col-lg-5">
-								<div class="post-image">
-									<div class="owl-carousel owl-theme" data-plugin-options="{'items':1}">
-										<div>
-											<div class="img-thumbnail d-block">
-												<img class="img-fluid" style="height: 40%;" src="<?php echo base_url(); ?>master/client/img/blog/blog-image-1.jpg" alt="">
-											</div>
-										</div>
-										<div>
-											<div class="img-thumbnail d-block">
-												<img class="img-fluid" src="<?php echo base_url(); ?>master/client/img/blog/blog-image-2.jpg" alt="">
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-7">
-
-								<div class="post-content">
-
-									<h2><a href="blog-post.html">Class aptent taciti sociosqu ad litora torquent</a></h2>
-									<p>Euismod atras vulputate iltricies etri elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nunc dui, tristique in semper vel, congue sed ligula. Nam dolor ligula, faucibus id sodales in, auctor fringilla libero. Pellentesque pellentesque tempor tellus eget hendrerit. Morbi id aliquam ligula. Aliquam id dui sem. Proin rhoncus consequat nisl, eu ornare mauris tincidunt vitae. [...]</p>
-
-								</div>
-							</div>
-
-						</div>
-						<div class="row">
-							<div class="col">
-								<div class="post-meta">
-									<span><i class="fas fa-calendar-alt"></i> January 10, 2017 </span>
-									<span><i class="fas fa-user"></i> By <a href="#">John Doe</a> </span>
-									<span><i class="fas fa-tag"></i> <a href="#">Duis</a>, <a href="#">News</a> </span>
-									<span><i class="fas fa-comments"></i> <a href="#">12 Comments</a></span>
-									<span class="d-block d-md-inline-block float-md-right mt-3 mt-md-0"><a href="blog-post.html" class="btn btn-xs btn-primary">Read more...</a></span>
-								</div>
-							</div>
-						</div>
-
-					</article>
 				</div>
 				<div class="tabs col-lg-3">
 					<h2><strong>Berita Lainnya</strong></h2>
