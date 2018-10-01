0ed66f0575762829a9bdd81a19b2f5356da5dcc3
DESKTOP-5E18HRJ\Biem <Abiemassyikin@gmail.com>
2018-10-01 22:57:47 +0700

    new admin dashboard

---
+++ application/views/admin/side/js.php
@@ -0,0 +1,30 @@
+<script src="<?php echo base_url() ?>master/adm/assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/popper/popper.min.js"></script>
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="<?php echo base_url() ?>master/adm/js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="<?php echo base_url() ?>master/adm/js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="<?php echo base_url() ?>master/adm/js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="<?php echo base_url() ?>master/adm/js/custom.min.js"></script>
+    <!-- ============================================================== -->
+    <!-- This page plugins -->
+    <!-- ============================================================== -->
+    <!-- chartist chart -->
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/chartist-js/dist/chartist.min.js"></script>
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
+    <!--c3 JavaScript -->
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/d3/d3.min.js"></script>
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/c3-master/c3.min.js"></script>
+    <!-- Chart JS -->
+    <script src="<?php echo base_url() ?>master/adm/js/dashboard1.js"></script>
+    <!-- ============================================================== -->
+    <!-- Style switcher -->
+    <!-- ============================================================== -->
+    <script src="<?php echo base_url() ?>master/adm/assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
