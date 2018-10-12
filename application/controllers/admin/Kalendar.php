594ffea1067706bd47ac5caa51d2dd51624eea9c
Christopher Nanda Jonathan <cnjonathan.cnj@gmail.com>
2018-10-12 20:23:40 +0700

    kalendar belum selese. dikit lagi..

--- application/controllers/admin/Kalendar.php
+++ application/controllers/admin/Kalendar.php
@@ -5,6 +5,22 @@ class Kalendar extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('admin/v_kalendar');
+		$data['agenda'] = $this->M_alamin->select('agenda');
+		$this->load->view('admin/v_kalendar', $data);
+	}
+
+	public function i_kalendar(){
+		$data['tanggal_agenda']=$this->input->post('tanggal_akademik');
+		$data['nama_kegiatan']=$this->input->post('kegiatan_akademik');
+		$this->M_alamin->insert('agenda', $data);
+		header('location:'.base_url().'admin/Kalendar');
+	}
+
+	public function e_kalendar(){
+		$id = $this->input->post('id_agenda');
+		$data['tanggal_agenda']=$this->input->post('tanggal_akademik');
+		$data['nama_kegiatan']=$this->input->post('kegiatan_akademik');
+		$this->M_alamin->update('agenda', $data);
+		header('location:'.base_url().'admin/Kalendar');
 	}
 }
--- application/views/admin/v_kalendar.php
+++ application/views/admin/v_kalendar.php
@@ -40,6 +40,89 @@
                 </div>
             </div>
         </div>
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Kalender Akademik</h4>
+                    <div id="responsive-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                    <h4 class="modal-title">Tambah Kegiatan</h4>
+                                </div>
+                                <form method="post" action="<?php echo base_url("admin/Kalendar/i_kalendar")?>" enctype="multipart/form-data">
+                                <div class="modal-body">
+                                        <div class="form-group">
+                                            <label for="recipient-name" class="control-label">Tanggal:</label>
+                                            <input type="text" class="form-control" name="tanggal_akademik">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="message-text" class="control-label">Kegiatan:</label>
+                                            <textarea type="text" class="form-control" name="kegiatan_akademik"></textarea>
+                                        </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-danger waves-effect waves-light">Save changes</button>
+                                </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="button" name="button" data-toggle="modal" data-target="#responsive-modal">Tambah</button>
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>No.</th>
+                                    <th>Tanggal</th>
+                                    <th>Kegiatan</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                              <?php $no = 1; foreach ($agenda->result() as $a) { ?>
+                                <tr>
+                                    <td><?php echo $no++; ?></td>
+                                    <td><?php echo $a->tanggal_agenda; ?></td>
+                                    <td><span class="peity-line" data-width="120" data-peity='{ "fill": ["#009efb"], "stroke":["#009efb"]}' data-height="40"><?php echo $a->nama_kegiatan; ?></span> </td>
+                                    <td><button data-toggle="modal" data-target="#responsive-modal-edit">edit</button><button>hapus</button></td>
+                              <?php } ?>
+                                    <div id="responsive-modal-edit" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                                    <h4 class="modal-title">Edit Kegiatan</h4>
+                                                </div>
+                                                <form method="post" action="<?php echo base_url("admin/Kalendar/e_kalendar")?>" enctype="multipart/form-data">
+                                                <div class="modal-body">
+                                                        <div class="form-group">
+                                                            <label for="recipient-name" class="control-label">Tanggal:</label>
+                                                            <input type="text" class="form-control" name="tanggal_akademik" value="<?php echo $agenda->tanggal_agenda; ?>">
+                                                            <input type="text" class="form-control" name="id_agenda" value="<?php echo $agenda->id_agenda; ?>" hidden>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label for="message-text" class="control-label">Kegiatan:</label>
+                                                            <textarea type="text" class="form-control" name="kegiatan_akademik"><?php echo $agenda->nama_kegiatan; ?></textarea>
+                                                        </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+                                                    <button type="submit" class="btn btn-danger waves-effect waves-light">Save changes</button>
+                                                </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
   <?php $this->load->view("admin/side/footer"); ?>
